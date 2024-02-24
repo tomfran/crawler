eae8ffb40dfb3557e811777d0718b0dbdd555afc
Francesco <tomaselli.fr@gmail.com>
2024-02-25 00:47:27 +0100

    fetch fingerprint

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,11 +1,17 @@
 use html5ever::tree_builder::TreeSink;
+use regex::Regex;
 use reqwest::blocking::get;
 use scraper::{Html, Selector};
+
+use crate::simhash::simhash;
+
+const SIMHASH_SHINGLE_SIZE: usize = 3;
 
 #[derive(Debug)]
 pub struct Webpage {
     pub url: String,
-    pub content: String,
+    pub raw_content: String,
+    pub fingerprint: u128,
     pub links: Vec<String>,
 }
 
@@ -13,9 +19,8 @@ pub fn fetch(url: &str) -> Option<Webpage> {
     let response = get(url).ok()?;
     let body = response.text().ok()?;
 
-    let mut document = Html::parse_document(&body);
+    let document = Html::parse_document(&body);
 
-    // extract all links, the input url is added to relative ones
     let a_sel = Selector::parse("a").unwrap();
     let url = url.to_string();
 
@@ -32,39 +37,45 @@ pub fn fetch(url: &str) -> Option<Webpage> {
         })
         .collect();
 
+    let raw_content = document.html();
+    let fingerprint = compute_digest(document);
+
+    Some(Webpage {
+        url,
+        raw_content,
+        fingerprint,
+        links,
+    })
+}
+
+fn compute_digest(mut document: Html) -> u128 {
     // remove scripts, styles and links
     let scripts_sel = &Selector::parse("script").unwrap();
     let style_sel = &Selector::parse("style").unwrap();
+    let a_sel = &Selector::parse("a").unwrap();
 
     let r1 = document.select(scripts_sel);
     let r2 = document.select(style_sel);
-    let r3 = document.select(&a_sel);
+    let r3 = document.select(a_sel);
 
     let node_ids: Vec<_> = r1.chain(r2).chain(r3).map(|x| x.id()).collect();
     for id in node_ids {
         document.remove_from_parent(&id);
     }
 
-    let content = document.html();
+    // remove all tags and non-asci chars
+    let re = Regex::new(r"<[^>]*>|[^\x00-\x7F]+").unwrap();
+    let strip = re.replace_all(document.html().as_str(), "").to_string();
 
-    Some(Webpage {
-        url,
-        content,
-        links,
-    })
+    simhash(strip, SIMHASH_SHINGLE_SIZE)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     pub fn test() {
-//         let d = fetch("https://tomfran.github.io/posts/lsm/").unwrap();
-
-//         println!(
-//             "Url: {}\n\nContent: {}\n\nLinks: {:?}",
-//             d.url, d.content, d.links
-//         );
-//     }
-// }
+    #[test]
+    pub fn test() {
+        assert!(fetch("https://tomfran.github.io/about/").is_some());
+    }
+}
